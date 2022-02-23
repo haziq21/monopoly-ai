27731c562fe6031e6e255af12556c0f9b217987c
Haziq Hairil <haziqhairil21@gmail.com>
2022-02-23 19:05:15 +0800

    Modify `Game` to fit state reimplementation

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -3,33 +3,41 @@ mod globals;
 mod agent;
 pub use agent::Agent;
 
-mod state;
-use state::StateDiff;
+mod state_diff;
+use state_diff::StateDiff;
 
+/// A simulation of Monopoly.
 pub struct Game {
-    pub agents: Vec<Agent>,
-    pub current_state: usize,
-    pub move_history: Vec<usize>,
-    child_diffs: Vec<StateDiff>,
+    /// Agents playing the game.
+    agents: Vec<Agent>,
+    /// The moves taken by players in terms of the indexes of the children.
+    move_history: Vec<usize>,
+    /// The current game state, as well as all its decendants.
+    state_nodes: Vec<StateDiff>,
+    /// Indexes of states that have been marked for deletion.
+    /// These states can be safely replaced by newer states.
+    dirty_states: Vec<usize>,
+    /// The index of the state the game is currently at.
+    current_state: usize,
 }
 
 impl Game {
     /*********        PUBLIC INTERFACES        *********/
 
-    /// Create a new game.
+    /// Return a new game.
     pub fn new(agents: Vec<Agent>) -> Self {
         let player_count = agents.len();
         Self {
             agents,
-            current_state: 0,
-            child_diffs: vec![StateDiff::new(player_count)],
             move_history: vec![],
+            state_nodes: vec![StateDiff::new(player_count)],
+            dirty_states: vec![],
+            current_state: 0,
         }
     }
 
     /// Play the game until it ends.
     pub fn play(&mut self) {
         // Placeholder
-        // self.agents[0].make_choice(&mut self.current_state, &self.move_history);
     }
 }
