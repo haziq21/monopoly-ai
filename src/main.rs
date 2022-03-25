76b4a350ed6b8c67bcb6714ee5384d76c7147c3b
Haziq Hairil <haziqhairil21@gmail.com>
2022-03-25 21:17:22 +0800

    Remove human player

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ use game::{Agent, Game};
 fn main() {
     let start = Instant::now();
 
-    Game::play(vec![
-        Agent::new_ai(1000, 2., 0),
-        Agent::new_human(),
-        Agent::new_random(),
-    ]);
+    Game::play(vec![Agent::new_ai(1000, 2., 0), Agent::new_random()]);
 
     let duration = start.elapsed();
 
